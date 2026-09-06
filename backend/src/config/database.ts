b561e348011ac9d6ed749b4c2d903c84f93f4884
Jose Luis <jl1533336@gmail.com>
2026-09-05 20:34:16 -0400

    estructura del proyecto de servicios telematicos
    se diseño la estructura/arcquitectura del proyecto de api de trivago de campus universitario

---
+++ backend/src/config/database.ts
@@ -0,0 +1,8 @@
+/**
+ * ARCHIVO: database.ts
+ * PROPÓSITO: Instanciar y exportar la conexión a la base de datos.
+ * Si usas Prisma, aquí exportarás el PrismaClient para que los servicios 
+ * lo importen y lo usen (Patrón Singleton).
+ */
+
+// export const prisma = new PrismaClient();
---
+++ backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,6 @@
+/**
+ * ARCHIVO: auth.service.ts
+ * PROPÓSITO: Lógica de negocio de Autenticación.
+ * Aquí verificas contraseñas (bcrypt), consultas la base de datos para ver 
+ * si el usuario existe, y generas los tokens JWT.
+ */
